d1e469f9e9e44784bc5cf98ac1747329c3477324
Asus <Asus@Irene>
2026-01-24 00:32:48 +0100

    VERSION FINAL FRANCIA

---
+++ src/main/webapp/sw.js
@@ -0,0 +1,24 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+const CACHE_NAME = 'familia-app-v1';
+const urlsToCache = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
